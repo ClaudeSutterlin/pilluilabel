6a01d2e9afcbbc4144b227cd411fea6e85ef3f5f
Will Clarke <willclarke@Will-Clarkes-2014-MacBook-Pro.local>
2016-11-10 14:59:42 -0800

    Initial commit

---
+++ PillUILabel/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  PillUILabel
+//
+//  Created by Will Clarke on 11/10/16.
+//  Copyright © 2016 Will Clarke. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
---
+++ README.md
@@ -0,0 +1,4 @@
+For more information, visit:
+
+http://www.willclarke.net/2016/11/10/pill-uilabel-using-ibdesignables/
+
